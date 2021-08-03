29ace272916dbb128218453242a3fe511948cab5
Karen Méndez <kmendezgz@gmail.com>
2021-08-02 22:04:42 -0600

    fixing route

--- routes/playlist.route.js
+++ routes/playlist.route.js
@@ -6,6 +6,6 @@ routerPlaylist.post('/playlists', playlistController.create);
 routerPlaylist.get('/playlists', playlistController.getPlaylist);
 routerPlaylist.put('/playlists', playlistController.update);
 routerPlaylist.delete('/playlists/:id', playlistController.delete);
-routerPlaylist.delete('/playlist/:id/song/:song', playlistController.deleteSong)
+routerPlaylist.delete('/playlists/:id/song/:song', playlistController.deleteSong)
 
 module.exports = routerPlaylist;
